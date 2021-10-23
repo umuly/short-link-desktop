b2d1e9a3e9b07d605246ef2bbed39e7b48d89191
Çağlar ÇAKMAK <caglar.cakmak@umuly.com>
2021-10-23 15:18:41 +0300

    v2

---
+++ Assets/KNOT Localization/Editor/Scripts/UI/Labels/IKnotKeyViewLabel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Knot.Localization.Editor
+{
+    public interface IKnotKeyViewLabel<TKeyView>
+    {
+        int Order { get; }
+
+        bool IsAssignableTo(TKeyView keyView);
+
+        GUIContent GetLabelContent(TKeyView keyView);
+    }
+}
