e957d5ce68488db6ab4b01a5fe14814e966455e1
Çağlar ÇAKMAK <caglar.cakmak@umuly.com>
2021-10-25 12:30:40 +0300

    Localization eklendi.

---
+++ Assets/KNOT Localization/Runtime/Scripts/Components/KnotLocalizedString.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Knot.Localization.Components
+{
+    /// <summary>
+    /// Component that allows to localize attached component's <see cref="string"/> properties and methods trough <see cref="System.Reflection"/>
+    /// </summary>
+    [AddComponentMenu(KnotLocalization.CoreName + "/Localized String", 1000)]
+    [DisallowMultipleComponent]
+    public partial class KnotLocalizedString : KnotLocalizedMethod<KnotTextKeyReference, string, KnotStringTargetMethod>
+    {
+        
+    }
+}
