b849e8f96ccd91196e1933eca33344bc9f046f47
Çağlar ÇAKMAK <caglar.cakmak@umuly.com>
2021-11-01 20:10:58 +0300

    DropdownTextMeshPro ya kod uygulandı.

--- Assets/KNOT Localization/LanguageSelector.cs
+++ Assets/KNOT Localization/LanguageSelector.cs
@@ -7,25 +7,36 @@ using System.Linq;
 using TMPro;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(TMP_Dropdown))]
+
 public class LanguageSelector : MonoBehaviour
 {
+    const string PrefName = "optionvalue";
+
+    private TMP_Dropdown _dropdown;
     void Start()
     {
-        Dropdown languageDropdown = GetComponent<Dropdown>();
+        TMP_Dropdown languageDropdown = GetComponent<TMP_Dropdown>();
 
         languageDropdown.AddOptions(KnotLocalization.Manager.Languages.Select(d => d.NativeName).ToList()); 
         languageDropdown.onValueChanged.AddListener(OnLanguageChanged);
-
+        _dropdown.value = PlayerPrefs.GetInt(PrefName, 0);
     }
     private void OnLanguageChanged(int arg0)
     {
         KnotLocalization.Manager.LoadLanguage(KnotLocalization.Manager.Languages[arg0]);
     }
-   
-    void Update()
+
+    void Awake()
     {
-        
+        _dropdown = GetComponent<TMP_Dropdown>();
+
+        _dropdown.onValueChanged.AddListener(new UnityAction<int>(index =>
+        {
+            PlayerPrefs.SetInt(PrefName, _dropdown.value);
+            PlayerPrefs.Save();
+        }));
     }
-    
-    
+
+
 }
