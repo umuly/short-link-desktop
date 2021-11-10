fbc387a806acd6bac6493e89db6db659e14243ec
Çağlar ÇAKMAK <caglar.cakmak@umuly.com>
2021-11-10 17:05:03 +0300

    Sürüm güncellemesi Bundle version code 25
    Dropdown artık seçilen dili getiriyor.

--- Assets/KNOT Localization/LanguageSelector.cs
+++ Assets/KNOT Localization/LanguageSelector.cs
@@ -16,11 +16,114 @@ public class LanguageSelector : MonoBehaviour
     private TMP_Dropdown _dropdown;
     void Start()
     {
-        TMP_Dropdown languageDropdown = GetComponent<TMP_Dropdown>();
+        int defultT = 0;
+        switch (Application.systemLanguage)
+        {
+            case SystemLanguage.Afrikaans:
+                break;
+            case SystemLanguage.Arabic:
+                break;
+            case SystemLanguage.Basque:
+                break;
+            case SystemLanguage.Belarusian:
+                break;
+            case SystemLanguage.Bulgarian:
+                break;
+            case SystemLanguage.Catalan:
+                break;
+            case SystemLanguage.Chinese:
+                break;
+            case SystemLanguage.Czech:
+                break;
+            case SystemLanguage.Danish:
+                break;
+            case SystemLanguage.Dutch:
+                break;
+            case SystemLanguage.English:
+                defultT = 0;
+                break;
+            case SystemLanguage.Estonian:
+                break;
+            case SystemLanguage.Faroese:
+                break;
+            case SystemLanguage.Finnish:
+                break;
+            case SystemLanguage.French:
+                defultT = 3;
+                break;
+            case SystemLanguage.German:
+                defultT = 2;
+                break;
+            case SystemLanguage.Greek:
+                break;
+            case SystemLanguage.Hebrew:
+                break;
+            case SystemLanguage.Hungarian:
+                break;
+            case SystemLanguage.Icelandic:
+                break;
+            case SystemLanguage.Indonesian:
+                break;
+            case SystemLanguage.Italian:
+                defultT = 6;
+                break;
+            case SystemLanguage.Japanese:
+                break;
+            case SystemLanguage.Korean:
+                break;
+            case SystemLanguage.Latvian:
+                break;
+            case SystemLanguage.Lithuanian:
+                break;
+            case SystemLanguage.Norwegian:
+                break;
+            case SystemLanguage.Polish:
+                break;
+            case SystemLanguage.Portuguese:
+                defultT = 5;
+                break;
+            case SystemLanguage.Romanian:
+                break;
+            case SystemLanguage.Russian:
+                defultT = 7;
+                break;
+            case SystemLanguage.SerboCroatian:
+                break;
+            case SystemLanguage.Slovak:
+                break;
+            case SystemLanguage.Slovenian:
+                break;
+            case SystemLanguage.Spanish:
+                defultT = 4;
+                break;
+            case SystemLanguage.Swedish:
+                break;
+            case SystemLanguage.Thai:
+                break;
+            case SystemLanguage.Turkish:
+                defultT = 1;
+                break;
+            case SystemLanguage.Ukrainian:
+                break;
+            case SystemLanguage.Vietnamese:
+                break;
+            case SystemLanguage.ChineseSimplified:
+                break;
+            case SystemLanguage.ChineseTraditional:
+                break;
+            case SystemLanguage.Unknown:
+                break;
+            default:
+                break;
+        }
 
+
+        TMP_Dropdown languageDropdown = GetComponent<TMP_Dropdown>();
         languageDropdown.AddOptions(KnotLocalization.Manager.Languages.Select(d => d.NativeName).ToList()); 
         languageDropdown.onValueChanged.AddListener(OnLanguageChanged);
-        _dropdown.value = PlayerPrefs.GetInt(PrefName, 0);
+
+        _dropdown.value = PlayerPrefs.GetInt(PrefName, defultT);
+
     }
     private void OnLanguageChanged(int arg0)
     {
